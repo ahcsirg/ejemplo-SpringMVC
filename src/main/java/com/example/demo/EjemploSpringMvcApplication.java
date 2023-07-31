3ccf4d38d40256561e378999e4484ce73a40a5ad
Alexis <a.santander.t@gmail.com>
2023-07-31 05:01:40 -0400

    @asantander Update Subida de proyecto

---
+++ src/main/java/com/example/demo/EjemploSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploSpringMvcApplication.class, args);
+	}
+
+}
